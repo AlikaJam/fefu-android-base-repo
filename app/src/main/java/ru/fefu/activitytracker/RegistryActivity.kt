13f99abbf72b173b116c824ac1607607b0fb1c01
AlikaJam <71496319+AlikaJam@users.noreply.github.com>
2021-12-02 21:30:21 +1000

    Create RegistryActivity.kt

---
+++ app/src/main/java/ru/fefu/activitytracker/RegistryActivity.kt
@@ -0,0 +1,14 @@
+package ru.fefu.activitytracker
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class RegistryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registry)
+    }
+    fun back(view: View) {
+        onBackPressed()
+    }
+}
