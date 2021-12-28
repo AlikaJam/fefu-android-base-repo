d43d57742366b05dfd3759ea710cbc35a431dd6b
AlikaJam <71496319+AlikaJam@users.noreply.github.com>
2021-12-28 12:37:56 +1000

    Update and rename app/src/main/java/ru/fefu/activitytracker/WelcomeScreen.kt to app/src/main/java/ru/fefu/activitytracker/main/WelcomeScreen.kt

---
+++ app/src/main/java/ru/fefu/activitytracker/main/WelcomeScreen.kt
@@ -0,0 +1,22 @@
+package ru.fefu.activitytracker.main
+
+import android.content.Intent
+
+import androidx.appcompat.app.AppCompatActivity
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+import ru.fefu.activitytracker.R
+
+class WelcomeScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome_screen)
+        val btnRegistration = findViewById<Button>(R.id.registrationButton)
+        btnRegistration.setOnClickListener {startActivity(Intent(this, Registration::class.java))}
+        val clickText = findViewById<TextView>(R.id.textQuestion)
+        clickText.setOnClickListener {startActivity(Intent(this, Login::class.java))}
+    }
+}
