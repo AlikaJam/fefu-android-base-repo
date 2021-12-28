e6857195e515f2d650f28d21ef6cbfe2ea50df22
AlikaJam <71496319+AlikaJam@users.noreply.github.com>
2021-12-28 12:41:24 +1000

    Update BaseFragment.kt

--- app/src/main/java/ru/fefu/activitytracker/BaseFragment.kt
+++ app/src/main/java/ru/fefu/activitytracker/BaseFragment.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.annotation.LayoutRes
+
+import androidx.anotation.LayoutRes
 import androidx.fragment.app.Fragment
 import androidx.viewbinding.ViewBinding
+
 import java.lang.reflect.ParameterizedType
 
 open class BaseFragment<V : ViewBinding>(@LayoutRes layoutResId: Int) : Fragment(layoutResId) {
