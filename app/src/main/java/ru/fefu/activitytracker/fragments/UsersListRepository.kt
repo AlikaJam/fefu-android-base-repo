654df6e2b4fe0f2bc6d02df8a15fa8ae336917e2
AlikaJam <71496319+AlikaJam@users.noreply.github.com>
2021-12-30 08:50:03 +1000

    fix

--- app/src/main/java/ru/fefu/activitytracker/fragments/UsersListRepository.kt
+++ app/src/main/java/ru/fefu/activitytracker/fragments/UsersListRepository.kt
@@ -5,7 +5,8 @@ import ru.fefu.activitytracker.ListItem
 class UsersListRepository {
     private val usersListItem = listOf(
         ListItem.Date(
-            date = "Вчера",),
+            date = "Вчера",
+        ),
         ListItem.Item(
             distance = "9.43 км",
             time = "2 часа 3 минуты",
@@ -13,7 +14,8 @@ class UsersListRepository {
             date = "12 часов назад",
             startTime = "11:40",
             endTime = "13:43",
-            user = "@coolfire"),
+            user = "@coolfire",
+        ),
         ListItem.Item(
             distance = "2 км",
             time = "40 минут",
@@ -21,7 +23,8 @@ class UsersListRepository {
             date = "18 часов назад",
             startTime = "12:03",
             endTime = "12:43",
-            user = "@hawk"),
+            user = "@hawk",
+        ),
         ListItem.Item(
             distance = "5 км",
             time = "1 час",
@@ -29,7 +32,9 @@ class UsersListRepository {
             date = "14 часов назад",
             startTime = "20:35",
             endTime = "21:35",
-            user = "@snowflake"),
+            user = "@snowflake",
+        ),
     )
+
     fun getItem() : List<ListItem> = usersListItem
 }
