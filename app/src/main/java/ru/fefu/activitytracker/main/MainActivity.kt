a87cc6c25ccf9e688ad0b5d36e860cbdfdd52f1f
AlikaJam <71496319+AlikaJam@users.noreply.github.com>
2021-12-30 08:43:30 +1000

    fix

--- app/src/main/java/ru/fefu/activitytracker/main/MainActivity.kt
+++ app/src/main/java/ru/fefu/activitytracker/main/MainActivity.kt
@@ -1,19 +1,20 @@
 package ru.fefu.activitytracker.main
 
 import androidx.appcompat.app.AppCompatActivity
-
 import android.os.Bundle
-
 import ru.fefu.activitytracker.R
 import ru.fefu.activitytracker.databinding.ActivityMainBinding
 import ru.fefu.activitytracker.fragments.*
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction().apply {
                 add(
@@ -24,6 +25,7 @@ class MainActivity : AppCompatActivity() {
                 commit()
             }
         }
+
         binding.bottomNav.setOnNavigationItemSelectedListener {
             val activityFragment = supportFragmentManager.findFragmentByTag("activityFragment")
             val profileFragment = supportFragmentManager.findFragmentByTag("profileFragment")
@@ -36,6 +38,7 @@ class MainActivity : AppCompatActivity() {
                         supportFragmentManager.beginTransaction().show(activityFragment).commit()
                     }
                 }
+
                 R.id.navigation_profile -> {
                     if (activityFragment !== null) {
                         supportFragmentManager.beginTransaction().hide(activityFragment).commit()
@@ -57,11 +60,21 @@ class MainActivity : AppCompatActivity() {
             true
         }
     }
+
     override fun onBackPressed() {
         val active = supportFragmentManager.fragments.firstOrNull{!it.isHidden}!!
         val childManager = active.childFragmentManager
-        if (childManager.backStackEntryCount != 0) {childManager.popBackStack()}
-        else if (supportFragmentManager.backStackEntryCount != 0) {supportFragmentManager.popBackStack()}
-        else {super.onBackPressed()}
+
+        if (childManager.backStackEntryCount != 0) {
+            childManager.popBackStack()
+        }
+
+        else if (supportFragmentManager.backStackEntryCount != 0) {
+            supportFragmentManager.popBackStack()
+        }
+
+        else {
+            super.onBackPressed()
+        }
     }
 }
