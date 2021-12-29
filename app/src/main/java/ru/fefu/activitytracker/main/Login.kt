8cc15c65da1b646dede6bac84826343fb82912dc
AlikaJam <71496319+AlikaJam@users.noreply.github.com>
2021-12-30 08:44:06 +1000

    fix

--- app/src/main/java/ru/fefu/activitytracker/main/Login.kt
+++ app/src/main/java/ru/fefu/activitytracker/main/Login.kt
@@ -1,16 +1,18 @@
 package ru.fefu.activitytracker.main
 
 import androidx.appcompat.app.AppCompatActivity
-
 import android.os.Bundle
 import android.view.View
-
 import ru.fefu.activitytracker.R
 
 class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
     }
-    fun back(view: View) {onBackPressed()}
+
+    fun back(view: View) {
+        onBackPressed()
+    }
 }
