26b3b1ca87e77924d9e4c67cce6fb0e3757a4a76
AlikaJam <71496319+AlikaJam@users.noreply.github.com>
2021-12-30 16:01:04 +1000

    try

--- app/src/main/java/ru/fefu/activitytracker/BaseFragment.kt
+++ app/src/main/java/ru/fefu/activitytracker/BaseFragment.kt
@@ -4,17 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
-import androidx.anotation.LayoutRes
+import androidx.annotation.LayoutRes
 import androidx.fragment.app.Fragment
 import androidx.viewbinding.ViewBinding
-
 import java.lang.reflect.ParameterizedType
 
 open class BaseFragment<V : ViewBinding>(@LayoutRes layoutResId: Int) : Fragment(layoutResId) {
+
     protected open val binding: V
         get() = _binding!!
+
     private var _binding: V? = null
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -23,10 +24,12 @@ open class BaseFragment<V : ViewBinding>(@LayoutRes layoutResId: Int) : Fragment
         return super.onCreateView(inflater, container, savedInstanceState)
             ?.also { _binding = provideViewBinding(it) }
     }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
     @Suppress("UNCHECKED_CAST")
     protected open fun provideViewBinding(view: View): V {
         val bindingType = (javaClass.genericSuperclass as ParameterizedType).actualTypeArguments[0]
@@ -34,4 +37,4 @@ open class BaseFragment<V : ViewBinding>(@LayoutRes layoutResId: Int) : Fragment
         val method = bindingClass.getMethod("bind", View::class.java)
         return method.invoke(null, view) as V
     }
-}
+} 
