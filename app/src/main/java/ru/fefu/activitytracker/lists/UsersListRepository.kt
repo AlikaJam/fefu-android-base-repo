a89b8bd060cea7a4bcbdd9036cef1fa225ac02ac
AlikaJam <71496319+AlikaJam@users.noreply.github.com>
2021-12-30 16:28:07 +1000

    try

--- app/src/main/java/ru/fefu/activitytracker/lists/UsersListRepository.kt
+++ app/src/main/java/ru/fefu/activitytracker/lists/UsersListRepository.kt
@@ -1,32 +1,46 @@
 package ru.fefu.activitytracker.lists
 
+import android.os.Build
+import androidx.annotation.RequiresApi
+import java.time.LocalDateTime
+
 class UsersListRepository {
+    @RequiresApi(Build.VERSION_CODES.O)
     private val usersListItem = listOf(
-        ListItem.Date(date = "Вчера"),
+        ListItem.Date(
+            date = "Вчера",
+        ),
         ListItem.Item(
+            id = 125,
             distance = "9.43 км",
             time = "2 часа 3 минуты",
             activity = "Бег",
             date = "12 часов назад",
-            startTime = "11:40",
-            endTime = "13:43",
-            user = "@coolfire"),
+            startTime = LocalDateTime.now(),
+            endTime = LocalDateTime.now(),
+            user = "@coolfire",
+        ),
         ListItem.Item(
+            id = 133,
             distance = "2 км",
             time = "40 минут",
             activity = "Велосипед",
             date = "18 часов назад",
-            startTime = "12:03",
-            endTime = "12:43",
-            user = "@hawk"),
+            startTime = LocalDateTime.now(),
+            endTime = LocalDateTime.now(),
+            user = "@hawk",
+        ),
         ListItem.Item(
+            id = 15,
             distance = "5 км",
             time = "1 час",
             activity = "Бег",
             date = "14 часов назад",
-            startTime = "20:35",
-            endTime = "21:35",
-            user = "@snowflake"),
+            startTime = LocalDateTime.now(),
+            endTime = LocalDateTime.now(),
+            user = "@snowflake",
+        ),
     )
+
     fun getItem() : List<ListItem> = usersListItem
 }
