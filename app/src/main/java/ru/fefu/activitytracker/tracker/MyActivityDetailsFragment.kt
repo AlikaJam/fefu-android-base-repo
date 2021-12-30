dced116d70590e9a155632cda9c2450bca3e5016
AlikaJam <71496319+AlikaJam@users.noreply.github.com>
2021-12-30 16:11:55 +1000

    try

--- app/src/main/java/ru/fefu/activitytracker/tracker/MyActivityDetailsFragment.kt
+++ app/src/main/java/ru/fefu/activitytracker/tracker/MyActivityDetailsFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
 import ru.fefu.activitytracker.App
 import ru.fefu.activitytracker.BaseFragment
 import ru.fefu.activitytracker.lists.ListItem
@@ -12,7 +11,6 @@ import ru.fefu.activitytracker.R
 import ru.fefu.activitytracker.adapters.ItemAdapter
 import ru.fefu.activitytracker.databinding.FragmentMyActivityDetailsBinding
 import ru.fefu.activitytracker.room.ActivityRoom
-
 import java.time.*
 
 class MyActivityDetailsFragment(id: Int) :
@@ -20,11 +18,13 @@ class MyActivityDetailsFragment(id: Int) :
     private var _binding: FragmentMyActivityDetailsBinding? = null
     override val binding get() = _binding!!
     private val details = App.INSTANCE.db.activityDao().getById(id)
+
     fun getDetails(value: ActivityRoom): ListItem.Item {
         val id = value.id
         val type = MyActivityFragment.getActivityType(value.type)
         val startDate = LocalDateTime.ofInstant(Instant.ofEpochMilli(value.startTime), ZoneId.systemDefault())
         val endDate = LocalDateTime.ofInstant(Instant.ofEpochMilli(value.endTime), ZoneId.systemDefault())
+
         val duration = Duration.between(startDate, endDate)
         var seconds: Long = Math.abs(duration.getSeconds())
         val hours = seconds / 3600
@@ -35,7 +35,10 @@ class MyActivityDetailsFragment(id: Int) :
         var time = ""
         if (hours > 0) {
             time = "%d %s %d %s".format(hours, hours_, minutes, minutes_)
-        } else {time = "%d %s".format(minutes, minutes_)}
+        } else {
+            time = "%d %s".format(minutes, minutes_)
+        }
+
         var date = ""
         if (LocalDate.now().isEqual(endDate.toLocalDate())) {
             date = Duration.between(endDate, LocalDateTime.now()).toHours().toString() +
@@ -49,12 +52,15 @@ class MyActivityDetailsFragment(id: Int) :
         val distance = "1000 м"
         return ListItem.Item(id, distance, time, date, type, startDate, endDate)
     }
+
     companion object {
         fun newInstance(id: Int): MyActivityDetailsFragment {
             return MyActivityDetailsFragment(id)
         }
     }
+
     private val detail = getDetails(details)
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.textMyDistance.text = detail.distance
@@ -62,15 +68,19 @@ class MyActivityDetailsFragment(id: Int) :
         val endTime = "%02d".format(detail.endTime.hour) + ":" + "%02d".format(detail.endTime.minute)
         binding.textMyStartTime.text = startTime
         binding.textMyFinishTime.text = endTime
+
         binding.textMyDate.text = detail.date
+
         binding.toolbarMy.title = detail.activity
         binding.toolbarMy.setNavigationOnClickListener {
             activity?.onBackPressed()
         }
+
         binding.toolbarMy.setOnMenuItemClickListener {
             true
         }
     }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -78,4 +88,5 @@ class MyActivityDetailsFragment(id: Int) :
         _binding = FragmentMyActivityDetailsBinding.inflate(inflater, container, false)
         return binding.root
     }
+
 }
