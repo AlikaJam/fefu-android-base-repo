7e21627e69b2d51913d1a5984c5ac22b4b2240ae
AlikaJam <71496319+AlikaJam@users.noreply.github.com>
2021-12-30 12:50:37 +1000

    fix

--- app/src/main/java/ru/fefu/activitytracker/fragments/UsersActivityFragment.kt
+++ app/src/main/java/ru/fefu/activitytracker/fragments/UsersActivityFragment.kt
@@ -1,15 +1,11 @@
 package ru.fefu.activitytracker.fragments
 
 import android.os.Bundle
-
 import androidx.fragment.app.Fragment
-
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
 import androidx.recyclerview.widget.LinearLayoutManager
-
 import ru.fefu.activitytracker.BaseFragment
 import ru.fefu.activitytracker.ItemAdapter
 import ru.fefu.activitytracker.ListItem
@@ -21,15 +17,19 @@ import ru.fefu.activitytracker.main.MainActivity
 class UsersActivityFragment : BaseFragment<FragmentUsersActivityBinding>(R.layout.fragment_users_activity) {
     private val usersListRepository = UsersListRepository()
     private val adapterItems = ItemAdapter(usersListRepository.getItem())
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         with(binding.rcView) {
             adapter = adapterItems
             layoutManager = LinearLayoutManager(requireContext())
         }
+
         adapterItems.setItemClickListener {
             val manager = activity?.supportFragmentManager?.findFragmentByTag("activityFragment")?.childFragmentManager
             manager?.beginTransaction()?.apply {
