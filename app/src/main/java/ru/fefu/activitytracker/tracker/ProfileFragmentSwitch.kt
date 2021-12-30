ed9e0d9fb296715412a6dc6dd8ff51106e9f8785
AlikaJam <71496319+AlikaJam@users.noreply.github.com>
2021-12-30 14:34:35 +1000

    fix

--- app/src/main/java/ru/fefu/activitytracker/tracker/ProfileFragmentSwitch.kt
+++ app/src/main/java/ru/fefu/activitytracker/tracker/ProfileFragmentSwitch.kt
@@ -4,20 +4,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
 import androidx.fragment.app.Fragment
-
 import ru.fefu.activitytracker.R
 import ru.fefu.activitytracker.databinding.FragmentProfileSwitchBinding
 
 class ProfileFragmentSwitch: Fragment() {
     private var _binding: FragmentProfileSwitchBinding? = null
+
     private val binding get() = _binding!!
+
     companion object {
         fun newInstance(): ActivityFragmentSwitch {
             return ActivityFragmentSwitch()
         }
     }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -26,6 +27,7 @@ class ProfileFragmentSwitch: Fragment() {
         _binding = FragmentProfileSwitchBinding.inflate(inflater, container, false)
         return binding.root
     }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         if (savedInstanceState == null) {
@@ -33,11 +35,13 @@ class ProfileFragmentSwitch: Fragment() {
                 add(
                     R.id.fragment_container_profile_switch,
                     ProfileFragment(),
-                    "profile")
+                    "profile"
+                )
                 commit()
             }
         }
     }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
