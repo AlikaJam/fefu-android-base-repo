67aeb3611bb6e34b71e4cd84f380ceb70dcf7ec1
AlikaJam <71496319+AlikaJam@users.noreply.github.com>
2021-12-30 16:04:39 +1000

    Update ChangePasswordFragment.kt

--- app/src/main/java/ru/fefu/activitytracker/tracker/ChangePasswordFragment.kt
+++ app/src/main/java/ru/fefu/activitytracker/tracker/ChangePasswordFragment.kt
@@ -17,7 +17,7 @@ class ChangePasswordFragment :
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+
         _binding = FragmentChangePasswordBinding.inflate(inflater, container, false)
         return binding.root
     }
