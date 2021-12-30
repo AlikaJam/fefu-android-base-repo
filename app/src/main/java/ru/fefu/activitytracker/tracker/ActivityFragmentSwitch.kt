dd90305171f26a778ceefb776f11922c7d951d7a
AlikaJam <71496319+AlikaJam@users.noreply.github.com>
2021-12-30 14:40:03 +1000

    fix

--- app/src/main/java/ru/fefu/activitytracker/tracker/ActivityFragmentSwitch.kt
+++ app/src/main/java/ru/fefu/activitytracker/tracker/ActivityFragmentSwitch.kt
@@ -4,19 +4,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
 import androidx.fragment.app.Fragment
-
 import ru.fefu.activitytracker.R
 import ru.fefu.activitytracker.databinding.FragmentActivitySwitchBinding
+
 class ActivityFragmentSwitch: Fragment() {
     private var _binding: FragmentActivitySwitchBinding? = null
+
     private val binding get() = _binding!!
+
     companion object {
         fun newInstance(): ActivityFragmentSwitch {
             return ActivityFragmentSwitch()
         }
     }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -24,7 +26,8 @@ class ActivityFragmentSwitch: Fragment() {
     ): View? {
         _binding = FragmentActivitySwitchBinding.inflate(inflater, container, false)
         return binding.root
-    }
+    } 
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         if (savedInstanceState == null) {
@@ -34,6 +37,7 @@ class ActivityFragmentSwitch: Fragment() {
             }
         }
     }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
