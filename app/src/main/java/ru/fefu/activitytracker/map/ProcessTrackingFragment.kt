ce82bd02f2f4e9ec44efe32dca720947f692b78a
AlikaJam <71496319+AlikaJam@users.noreply.github.com>
2021-12-30 14:42:56 +1000

    fix

--- app/src/main/java/ru/fefu/activitytracker/map/ProcessTrackingFragment.kt
+++ app/src/main/java/ru/fefu/activitytracker/map/ProcessTrackingFragment.kt
@@ -1,14 +1,11 @@
 package ru.fefu.activitytracker.map
 
 import android.os.Bundle
-
 import androidx.fragment.app.Fragment
-
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
-
 import ru.fefu.activitytracker.BaseFragment
 import ru.fefu.activitytracker.R
 import ru.fefu.activitytracker.databinding.FragmentProcessTrackingBinding
@@ -18,19 +15,25 @@ class ProcessTrackingFragment :
 BaseFragment<FragmentProcessTrackingBinding>(R.layout.fragment_process_tracking) {
     private var _binding: FragmentProcessTrackingBinding? = null
     override val binding get() = _binding!!
+
     companion object {
         fun newInstance(): ProcessTrackingFragment {
             return ProcessTrackingFragment()
         }
     }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         val btnFinish: ImageButton = view.findViewById(R.id.button_finish)
         btnFinish.setOnClickListener {
             parentFragmentManager.popBackStack()
         }
-        binding.buttonFinish.setOnClickListener {}
+        binding.buttonFinish.setOnClickListener {
+
+        }
     }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
