3f405259041c1cc2f23b9489196bac7fc9ef56f8
AlikaJam <71496319+AlikaJam@users.noreply.github.com>
2021-12-30 16:01:46 +1000

    try

--- app/src/main/java/ru/fefu/activitytracker/App.kt
+++ app/src/main/java/ru/fefu/activitytracker/App.kt
@@ -1,15 +1,15 @@
 package ru.fefu.activitytracker
 
 import android.app.Application
-
 import androidx.room.Room
-
 import ru.fefu.activitytracker.room.ActivityDataBase
 
 class App: Application() {
+
     companion object {
         lateinit var INSTANCE: App
     }
+
     val db by lazy {
         Room.databaseBuilder(
             this,
@@ -17,8 +17,10 @@ class App: Application() {
             "my_database"
         ).allowMainThreadQueries().build()
     }
+
     override fun onCreate() {
         super.onCreate()
+
         INSTANCE = this
     }
 }
