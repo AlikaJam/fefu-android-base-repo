d240270c06a71dd9b6e9e75058e7c114c7c2c610
AlikaJam <71496319+AlikaJam@users.noreply.github.com>
2021-12-30 14:39:23 +1000

    fix

--- app/src/main/java/ru/fefu/activitytracker/tracker/ChangePasswordFragment.kt
+++ app/src/main/java/ru/fefu/activitytracker/tracker/ChangePasswordFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
 import ru.fefu.activitytracker.BaseFragment
 import ru.fefu.activitytracker.R
 import ru.fefu.activitytracker.databinding.FragmentChangePasswordBinding
@@ -13,6 +12,7 @@ class ChangePasswordFragment :
     BaseFragment<FragmentChangePasswordBinding>(R.layout.fragment_change_password) {
     private var _binding: FragmentChangePasswordBinding? = null
     override val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -20,10 +20,12 @@ class ChangePasswordFragment :
         _binding = FragmentChangePasswordBinding.inflate(inflater, container, false)
         return binding.root
     }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.toolbar2.setNavigationOnClickListener() {
             activity?.onBackPressed()
         }
     }
+
 }
