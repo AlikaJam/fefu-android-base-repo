0c317c293b10c3470517621bfd845bbae7c7623b
AlikaJam <71496319+AlikaJam@users.noreply.github.com>
2021-12-30 14:51:17 +1000

    fix

--- app/src/main/java/ru/fefu/activitytracker/adapters/ViewPagerAdapter.kt
+++ app/src/main/java/ru/fefu/activitytracker/adapters/ViewPagerAdapter.kt
@@ -4,7 +4,6 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
-
 import ru.fefu.activitytracker.tracker.ActivityFragment
 import ru.fefu.activitytracker.tracker.MyActivityFragment
 import ru.fefu.activitytracker.tracker.UsersActivityFragment
@@ -13,9 +12,15 @@ class ViewPagerAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle) :
     override fun getItemCount(): Int = 2
     override fun createFragment(position: Int): Fragment {
         return when(position) {
-            0->{MyActivityFragment()}
-            1->{UsersActivityFragment()}
-            else->{ActivityFragment()}
+            0->{
+                MyActivityFragment()
+            }
+            1->{
+                UsersActivityFragment()
+            }
+            else->{
+                ActivityFragment()
+            }
         }
     }
 }
