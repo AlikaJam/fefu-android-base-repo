fc3e5be617f5584abe39364098b72f4463bc5efa
AlikaJam <71496319+AlikaJam@users.noreply.github.com>
2021-12-30 16:02:40 +1000

    try

--- app/src/main/java/ru/fefu/activitytracker/tracker/ActivityFragment.kt
+++ app/src/main/java/ru/fefu/activitytracker/tracker/ActivityFragment.kt
@@ -2,20 +2,15 @@ package ru.fefu.activitytracker.tracker
 
 import android.content.Intent
 import android.os.Bundle
-
 import androidx.fragment.app.Fragment
-
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
 import android.widget.TableLayout
-
 import androidx.viewpager2.widget.ViewPager2
-
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
-
 import ru.fefu.activitytracker.R
 import ru.fefu.activitytracker.adapters.ViewPagerAdapter
 import ru.fefu.activitytracker.databinding.FragmentActivityBinding
@@ -27,6 +22,7 @@ class ActivityFragment : Fragment() {
     private lateinit var tabLayout: TableLayout
     private val _binding: FragmentActivityBinding? = null
     private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -39,14 +35,20 @@ class ActivityFragment : Fragment() {
         viewPager2.adapter = adapter
         TabLayoutMediator(tabLayout, viewPager2) {tab, position->
             when(position) {
-                0->{tab.text = "Моя"}
-                1->{tab.text = "Пользователей"}
+                0->{
+                    tab.text = "Моя"
+                }
+                1->{
+                    tab.text = "Пользователей"
+                }
             }
         }.attach()
+
         val btnFab = myFragment.findViewById<ImageButton>(R.id.button_fab)
         btnFab.setOnClickListener {
             startActivity(Intent(requireActivity(), NewActivity::class.java))
         }
+
         return myFragment
     }
 }
