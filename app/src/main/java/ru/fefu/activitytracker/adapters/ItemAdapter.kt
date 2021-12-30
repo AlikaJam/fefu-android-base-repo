8e0e8b30dfb1ed001ecbe0a6827cb04baa37405f
AlikaJam <71496319+AlikaJam@users.noreply.github.com>
2021-12-30 16:32:15 +1000

    try

--- app/src/main/java/ru/fefu/activitytracker/adapters/ItemAdapter.kt
+++ app/src/main/java/ru/fefu/activitytracker/adapters/ItemAdapter.kt
@@ -3,24 +3,25 @@ package ru.fefu.activitytracker.adapters
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
 import androidx.recyclerview.widget.RecyclerView
-
 import ru.fefu.activitytracker.lists.ListItem
 import ru.fefu.activitytracker.R
 import ru.fefu.activitytracker.databinding.ItemDataBinding
 import ru.fefu.activitytracker.databinding.ModelItemBinding
 
-class ItemAdapter(items: List<ListItem>): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-    private val list = items.toMutableList()
+class ItemAdapter(private val list: List<ListItem>): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
     private var itemClickListener: (Int) -> Unit = {}
+
     fun setItemClickListener(listener: (Int) -> Unit) {
         itemClickListener = listener
     }
+
     override fun getItemViewType(position: Int): Int {
         return if (list[position] is ListItem.Item) R.layout.model_item
                else R.layout.item_data
     }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         return if (viewType == R.layout.model_item) {
             val view = LayoutInflater.from(parent.context).inflate(R.layout.model_item, parent, false)
@@ -30,17 +31,39 @@ class ItemAdapter(items: List<ListItem>): RecyclerView.Adapter<RecyclerView.View
             ViewHolderDate(view)
         } else throw IllegalArgumentException("Invalid viewType")
     }
+
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         when(holder) {
             is ViewHolder -> holder.bind(list[position] as ListItem.Item)
             is ViewHolderDate -> holder.bind(list[position] as ListItem.Date)
         }
     }
+
     override fun getItemCount(): Int {
         return list.size
     }
+
+    companion object {
+        fun getNoun(number: Long, one: String, two: String, five: String) : String {
+            var n = number
+            n %= 100
+            if (n in 5..20) {
+                return five
+            }
+            n %= 10
+            if (n == 1L) {
+                return one
+            }
+            if (n in 2..4) {
+                return two
+            }
+            return five
+        }
+    }
+
     inner class ViewHolder(item: View): RecyclerView.ViewHolder(item) {
         private val binding = ModelItemBinding.bind(item)
+
         init {
             item.setOnClickListener {
                 val position = adapterPosition
@@ -48,6 +71,7 @@ class ItemAdapter(items: List<ListItem>): RecyclerView.Adapter<RecyclerView.View
                     itemClickListener.invoke(position)
             }
         }
+
         fun bind(model: ListItem.Item) = with(binding) {
             textDistance.text = model.distance
             textTime.text = model.time
@@ -56,8 +80,10 @@ class ItemAdapter(items: List<ListItem>): RecyclerView.Adapter<RecyclerView.View
             textUsers.text = model.user
         }
     }
+
     inner class ViewHolderDate(itemView: View): RecyclerView.ViewHolder(itemView) {
         private val binding = ItemDataBinding.bind(itemView)
+
         fun bind(date: ListItem.Date) = with(binding) {
             textDateShablon.text = date.date
         }
