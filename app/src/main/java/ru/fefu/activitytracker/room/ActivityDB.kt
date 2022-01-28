11fbc5625422ed6ac272df581e97eeea69a71df3
AlikaJam <71496319+AlikaJam@users.noreply.github.com>
2022-01-28 12:38:16 +1000

    homework-6

--- app/src/main/java/ru/fefu/activitytracker/room/ActivityDB.kt
+++ app/src/main/java/ru/fefu/activitytracker/room/ActivityDB.kt
@@ -1,9 +1,10 @@
 package ru.fefu.activitytracker.room
+
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 
-@Database(entities = [Activity::class], version = 1)
+@Database(entities = [ActivityRoom::class], version = 1)
 
 @TypeConverters(DataConverter::class)
 abstract class ActivityDB: RoomDatabase() {
