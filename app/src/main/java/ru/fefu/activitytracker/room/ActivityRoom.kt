7d9b4f97ead52f2d36eeb4dc43315a4e92312749
AlikaJam <71496319+AlikaJam@users.noreply.github.com>
2022-01-31 15:17:38 +1000

    Update ActivityRoom.kt

--- app/src/main/java/ru/fefu/activitytracker/room/ActivityRoom.kt
+++ app/src/main/java/ru/fefu/activitytracker/room/ActivityRoom.kt
@@ -9,7 +9,7 @@ data class ActivityRoom (
     @ColumnInfo(name = "activity_type") val type: Int,
     @ColumnInfo(name = "date_start") val dateStart: Long,
     @ColumnInfo(name = "date_end") val dateEnd: Long,
-    @ColumnInfo(name = "coordinates") val coordinates: List<Pair<Double,Double>>, //latitude and longitude
+    @ColumnInfo(name = "coordinates") val coordinates: List<Pair<Double,Double>>,
     @ColumnInfo(name = "finished") val finished: Int,
     @ColumnInfo(name = "distance") val distance: Double,
     @ColumnInfo(name = "comment") val comment: String,
